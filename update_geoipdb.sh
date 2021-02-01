ee4225e91349b4fef93af2bbb55bc968c2f209d8
Patrik Jonsson <patrik.jonsson@gmail.com>
2021-02-01 10:43:35 +0100

    Updating shell script according to https://www.shellcheck.net recommendations

--- update_geoipdb.sh
+++ update_geoipdb.sh
@@ -8,18 +8,18 @@ GEOIP_MD5="$1.md5"
 mkdir -p $TEMP_FOLDER
 
 if [ -f "$UPLOAD_FOLDER/$GEOIP_FILE" ] && [ -f "$UPLOAD_FOLDER/$GEOIP_MD5" ]; then
-  mv $UPLOAD_FOLDER/$GEOIP_FILE $TEMP_FOLDER
-  mv $UPLOAD_FOLDER/$GEOIP_MD5 $TEMP_FOLDER
+  mv "$UPLOAD_FOLDER/$GEOIP_FILE" "$TEMP_FOLDER"
+  mv "$UPLOAD_FOLDER/$GEOIP_MD5" "$TEMP_FOLDER"
 else
   echo "Could not find $UPLOAD_FOLDER/$GEOIP_FILE and $UPLOAD_FOLDER/$GEOIP_MD5"
   exit 1
 fi
 
-cd $TEMP_FOLDER
+cd "$TEMP_FOLDER"  || exit 1
 
 # Test the file integrity
-if md5sum --status -c $GEOIP_MD5; then
-  unzip $GEOIP_FILE
+if md5sum --status -c "$GEOIP_MD5"; then
+  unzip "$GEOIP_FILE"
   for filename in *.rpm; do
       geoip_update_data -f "$filename"
   done
